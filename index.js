5fcd90d9bb3d34e1d26e141eafbc79355cbb5ea4
king-handy <a.king.life@pm.me>
2021-02-28 12:52:42 -0500

    gradeFunction

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ Do the following:
    HINT: no function required
 */
 
-let votingAge = 18;
+const votingAge = 18;
 if (votingAge > 17) {
-  console.log(true);
+  console.log('true');
 }
 
 /*
@@ -156,7 +156,7 @@ Use the game function below to do the following:
   
   HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
-let computer = Math.round(Math.random());
+let computer = Math.random();
 function game(user, computer){
     if (user > computer){
       return `you win!`;
@@ -232,9 +232,23 @@ Using the grade function below do the following:
    below should return 'you got an F'
 */
   
-function grade(/*Your Code here */){
-  /*Your Code here */
+function grade(score){
+  if (score > 89){
+    return `you got an A`;
   }
+  else if (score > 79 && score < 90){
+    return `you got a B`;
+  }
+  else if (score > 69 && score < 80){
+    return `you got a C`;
+  }
+  else if (score > 59 && score < 70){
+    return `you got a D`;
+  }
+  else if (score < 60){
+    return `you got an F`;
+  }
+}
   
   
 
